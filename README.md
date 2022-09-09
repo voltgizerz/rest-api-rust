a214153ef863dc238b5efea3be6f19dc2b5bee73
volgizerz <felix.asui@gmail.com>
2022-09-10 01:09:37 +0700

    Add new endpoint

--- README.md
+++ README.md
@@ -8,8 +8,21 @@ Simple REST APIs.
 - run this project using <b>cargo run</b>
 
 # GET
-- Response :
+Routes:
+- (hello) GET /
+- (ping) GET /ping
+- (loops) GET /loops
+- (user) GET /user/<name>/<age>
+- (hello_param) GET /hello/<name>/<age>
+
+- Response hello_param:
 
 ```json 
-
+{
+    "active": true,
+    "username": "burhan",
+    "email": "burhan@gggmail.com",
+    "age": 12,
+    "duration": "Millis: 0 ms"
+}
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,88 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
+use std::time::Instant;
 #[macro_use] extern crate rocket;
 
+
+fn elapsed_time(before: std::time::Instant)-> String {
+    let elapsed = format!("Millis: {} ms", before.elapsed().as_millis());
+    elapsed
+}
+
 #[get("/")]
-fn index() -> &'static str {
+fn hello() -> &'static str {
     "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+#[derive(Serialize, Deserialize)]
+struct Ping {
+    text: String,
+    duration: String,
+}
+
+#[get("/ping")]
+fn ping() -> Json<Ping> {
+    let before = Instant::now();
+
+    Json(Ping {
+        text: "Pong!".to_string(),
+        duration: elapsed_time(before), 
+    })
+}
+
+#[get("/hello/<name>/<age>")]
+fn hello_param(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    age: u64,
+    duration: String,
+}
+
+#[get("/user/<name>/<age>")]
+fn user(name: &str, age: u64) -> Json<User> {
+    let before = Instant::now();
+
+    Json(User {
+        active: true,
+        username: name.to_string(),
+        email: format!("{}@email.com", name),
+        age: age,
+        duration: elapsed_time(before), 
+    })
+}
+
+#[derive(Serialize, Deserialize)]
+struct LoopResponse {
+    duration: String,
+}
+
+#[get("/loops")]
+fn loops() -> Json<LoopResponse> {
+    let before = Instant::now();
+    
+    println!("Start looping...");
+    
+    let mut sum = 0;
+    for n in 1..1000 {
+        sum += n;
+    }
+
+    println!("Total sum {}", sum);
+    
+    Json(LoopResponse {
+        duration: elapsed_time(before)
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![
+        hello, hello_param, user, loops, ping
+    ])
 }
